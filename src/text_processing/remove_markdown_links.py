a693b7471810ac2ff0fe9f1903abbe34477c74e6
Ben Crestel <ben.crestel@zoho.com>
2024-12-01 21:40:55 -0500

    finish email fetching

--- src/text_processing/remove_markdown_links.py
+++ src/text_processing/remove_markdown_links.py
@@ -1,35 +1,38 @@
 import re
 
+
 def find_md_links(md):
     """Returns dict of links in markdown:
     'regular': [foo](some.url)
     'footnotes': [foo][3]
-    
+
     [3]: some.url
     """
     # https://stackoverflow.com/a/30738268/2755116
 
-    INLINE_LINK_RE = re.compile(r'\[([^\]]+)\]\(([^)]+)\)')
-    FOOTNOTE_LINK_TEXT_RE = re.compile(r'\[([^\]]+)\]\[(\d+)\]')
-    FOOTNOTE_LINK_URL_RE = re.compile(r'\[(\d+)\]:\s+(\S+)')
+    INLINE_LINK_RE = re.compile(r"\[([^\]]+)\]\(([^)]+)\)")
+    FOOTNOTE_LINK_TEXT_RE = re.compile(r"\[([^\]]+)\]\[(\d+)\]")
+    FOOTNOTE_LINK_URL_RE = re.compile(r"\[(\d+)\]:\s+(\S+)")
 
     links = list(INLINE_LINK_RE.findall(md))
     footnote_links = dict(FOOTNOTE_LINK_TEXT_RE.findall(md))
     footnote_urls = dict(FOOTNOTE_LINK_URL_RE.findall(md))
 
     footnotes_linking = []
-        
-    for key in footnote_links.keys():
-        footnotes_linking.append((footnote_links[key], footnote_urls[footnote_links[key]]))
 
-    return {'regular': links, 'footnotes': footnotes_linking}
+    for key in footnote_links.keys():
+        footnotes_linking.append(
+            (footnote_links[key], footnote_urls[footnote_links[key]])
+        )
+
+    return {"regular": links, "footnotes": footnotes_linking}
 
 
 def replace_md_links(md, f):
     """Replace links url to f(url)"""
-    
+
     links = find_md_links(md)
     newmd = md
 
-    for r in links['regular']:
+    for r in links["regular"]:
         newmd = newmd.replace(r[1], f(r[1]))
--- src/utils/gmail.py
+++ src/utils/gmail.py
@@ -1,8 +1,9 @@
+import base64
 import logging
 from pathlib import Path
 from typing import List, TypedDict
-import base64
 
+from bs4 import BeautifulSoup
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 
@@ -28,40 +29,67 @@ class Email(TypedDict):
 class Gmail:
     def __init__(self) -> None:
         creds = Credentials.from_authorized_user_file(PATH_TO_ROOT / TOKEN_PATH, SCOPES)
-        self.service = build('gmail', 'v1', credentials=creds)
-    
-    def fetch_emails(self, sender: str, after: str = "1900/12/31", before: str = "2100/12/31") -> List[Email]:
+        self.service = build("gmail", "v1", credentials=creds)
+
+    def fetch_emails(
+        self, sender: str, after: str = "1900/12/31", before: str = "2100/12/31"
+    ) -> List[Email]:
         query = f"after:{after} before:{before} from:{sender}"
-        results = self.service.users().messages().list(userId='me', q=query).execute()
-        messages = results.get('messages', [])
-        return [self.create_email_from_message(message=message) for message in messages]
+        results = self.service.users().messages().list(userId="me", q=query).execute()
+        messages = results.get("messages", [])
+        emails = [
+            self.create_email_from_message(message=message) for message in messages
+        ]
+        return [email for email in emails if email["sender"] == sender]
 
     def create_email_from_message(self, message: dict) -> Email:
-        email_id = message['id']
-        message = self.service.users().messages().get(userId='me', id=email_id).execute()
-        headers = message['payload']['headers']
-        return Email(
-            sender = next(header['value'] for header in headers if header['name'] == 'From'),
-            subject = next(header['value'] for header in headers if header['name'] == 'Subject'),
-            date_utc = next(header['value'] for header in headers if header['name'] == 'Date'),
-            id = email_id,
-            text = self.get_text_from_message(message=message)
+        email_id = message["id"]
+        message = (
+            self.service.users().messages().get(userId="me", id=email_id).execute()
         )
-    
+        headers = message["payload"]["headers"]
+        return Email(
+            sender=next(
+                header["value"] for header in headers if header["name"] == "From"
+            ),
+            subject=next(
+                header["value"] for header in headers if header["name"] == "Subject"
+            ),
+            date_utc=next(
+                header["value"] for header in headers if header["name"] == "Date"
+            ),
+            id=email_id,
+            text=self.get_text_from_message(message=message),
+        )
+
     def get_text_from_message(self, message: dict) -> str:
-        mimeType = message['payload']['mimeType']
-        if mimeType == 'multipart/alternative':
+        mimeType = message["payload"]["mimeType"]
+        if mimeType == "multipart/alternative":
             # for a multipart email, look for the plain text part
-            for pp in message['payload']['parts']:
-                if pp['mimeType'] == 'text/plain':
-                    body_data = pp['body']['data']
+            for pp in message["payload"]["parts"]:
+                if pp["mimeType"] == "text/plain":
+                    body_data = pp["body"]["data"]
                     return base64.urlsafe_b64decode(body_data).decode()
+        elif mimeType == "text/plain":
+            body_data = message["payload"]["body"]["data"]
+            return base64.urlsafe_b64decode(body_data).decode()
+        elif mimeType == "text/html":
+            body_data = message["payload"]["body"]["data"]
+            text_html = base64.urlsafe_b64decode(body_data).decode()
+            return BeautifulSoup(text_html, "html.parser").get_text()
         else:
-            # TODO: Handle this case as some emails have text/html mimeType
-            headers = message['payload']['headers']
-            sender = next(header['value'] for header in headers if header['name'] == 'From')
-            subject = next(header['value'] for header in headers if header['name'] == 'Subject')
-            date_utc = next(header['value'] for header in headers if header['name'] == 'Date')
-            email_id = message['id']
-            logger.error(f"email {email_id}, from: {sender}, with subject: {subject}, sent on {date_utc} has mimeType: {mimeType}")
+            headers = message["payload"]["headers"]
+            sender = next(
+                header["value"] for header in headers if header["name"] == "From"
+            )
+            subject = next(
+                header["value"] for header in headers if header["name"] == "Subject"
+            )
+            date_utc = next(
+                header["value"] for header in headers if header["name"] == "Date"
+            )
+            email_id = message["id"]
+            logger.error(
+                f"email {email_id}, from: {sender}, with subject: {subject}, sent on {date_utc} has mimeType: {mimeType}"
+            )
             raise NotImplementedError
