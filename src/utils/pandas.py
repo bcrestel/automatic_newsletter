250c42dd5a7e2cd7074b77f1e262495aaf7e267a
Ben Crestel <ben.crestel@zoho.com>
2024-12-06 00:05:19 -0500

    convert list of news stories to dataframe

--- src/utils/pandas.py
+++ src/utils/pandas.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import List
 
 import pandas as pd
 
@@ -12,3 +13,15 @@ def print_dataframe_col_per_alphanumeric(df: pd.DataFrame, column: str):
     # Print the grouped entries
     for first_char, entries in sorted(groups.items()):
         print(f"{first_char} ({len(entries)}): {', '.join(entries)}")
+
+
+def convert_list_of_dict_to_dataframe(list_of_dict: List[dict], dict_keys: List) -> pd.DataFrame:
+    list_results = [[] for _ in range(len(dict_keys))]
+    for ns in list_of_dict:
+        for idx, key in enumerate(dict_keys):
+            try:
+                res = ns[key]
+            except KeyError as e:
+                res = ''
+            list_results[idx].append(res)
+    return pd.DataFrame({key: list_results[idx] for idx, key in enumerate(dict_keys)})
