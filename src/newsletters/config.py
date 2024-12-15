6569c923d1ba5940c3a1e0d2c9ddadb4bb2cb26e
Ben Crestel <ben.crestel@zoho.com>
2024-12-14 20:11:18 -0500

    recreate token automatically

--- src/newsletters/config.py
+++ src/newsletters/config.py
@@ -4,5 +4,6 @@ from src.newsletters.parser.tldr import tldr_parser
 
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 TOKEN_PATH = Path("secrets_vault/token.json")
+CREDENTIAL_PATH = Path("secrets_vault/credentials.json")
 
 NEWSLETTER_AND_PARSER = {"TLDR AI <dan@tldrnewsletter.com>": tldr_parser}
--- src/newsletters/gmail.py
+++ src/newsletters/gmail.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import base64
 import logging
 from pathlib import Path
@@ -5,10 +7,13 @@ from typing import List, TypedDict
 
 from bs4 import BeautifulSoup
 from google.oauth2.credentials import Credentials
+from google.auth.exceptions import RefreshError
 from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-from src.newsletters.config import SCOPES, TOKEN_PATH
+from src.newsletters.config import SCOPES, TOKEN_PATH, CREDENTIAL_PATH
 from src.newsletters.email import Email
+from src.utils.date import get_date_YYYY_MM_DD
 
 PATH_TO_ROOT = Path("../..")
 
@@ -17,15 +22,27 @@ logger = logging.getLogger(__name__)
 
 class Gmail:
     def __init__(self) -> None:
-        creds = Credentials.from_authorized_user_file(PATH_TO_ROOT / TOKEN_PATH, SCOPES)
-        self.service = build("gmail", "v1", credentials=creds)
+        self.path_to_token = PATH_TO_ROOT / TOKEN_PATH
+        self.service = self.create_services(path_to_token=self.path_to_token, scopes=SCOPES)
+
+    @staticmethod
+    def create_services(path_to_token: str, scopes: str):
+        creds = Credentials.from_authorized_user_file(filename=path_to_token, scopes=scopes)
+        return build("gmail", "v1", credentials=creds)
 
     def fetch_emails(
         self, sender: str, after: str = "1900/12/31", before: str = "2100/12/31"
     ) -> List[Email]:
         logger.debug(f"Fetch email for sender {sender} from {after} until {before}.")
         query = f"after:{after} before:{before} from:{sender}"
-        results = self.service.users().messages().list(userId="me", q=query).execute()
+        try:
+            results = self.service.users().messages().list(userId="me", q=query).execute()
+        except RefreshError as e:
+            logger.warning("Your Google token has expired. We'll create a new one.")
+            path_to_credentials = PATH_TO_ROOT / CREDENTIAL_PATH
+            self.recreate_token(path_to_token=self.path_to_token, path_to_credentials=path_to_credentials, scopes=SCOPES)
+            self.service = self.create_services(path_to_token=self.path_to_token, scopes=SCOPES)
+            results = self.service.users().messages().list(userId="me", q=query).execute()
         messages = results.get("messages", [])
         emails = [
             self.create_email_from_message(message=message) for message in messages
@@ -83,3 +100,16 @@ class Gmail:
                 f"email {email_id}, from: {sender}, with subject: {subject}, sent on {date_utc} has mimeType: {mimeType}"
             )
             raise NotImplementedError
+
+    @staticmethod
+    def recreate_token(path_to_token: str, path_to_credentials: str, scopes: list[str]):
+        # move existing token to delete folder
+        path_to_delete_folder = Path(os.path.dirname(path_to_token), "delete")
+        if not os.path.isdir(path_to_delete_folder):
+            path_to_delete_folder.mkdir(parents=True, exist_ok=False)
+        shutil.move(path_to_token, Path(path_to_delete_folder, f"token_{get_date_YYYY_MM_DD()}.json"))
+        # re-create new token
+        flow = InstalledAppFlow.from_client_secrets_file(path_to_credentials, scopes)
+        creds = flow.run_local_server(port=8080, bind_addr='0.0.0.0')
+        with open(path_to_token, "w") as token:
+            token.write(creds.to_json())
