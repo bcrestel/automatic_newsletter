3221744fa60d243d8e4ee579c29c0fa4289bfe5c
Ben Crestel <ben.crestel@zoho.com>
2024-12-26 10:03:31 -0500

    start report creation

--- src/scoring/main.py
+++ src/scoring/main.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from typing import Dict, List, Tuple
 
 import pandas as pd
 
@@ -17,7 +17,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def runner(list_news_stories: List[NewsStory]) -> pd.DataFrame:
+def runner(list_news_stories: List[NewsStory]) -> Tuple[pd.DataFrame, Dict]:
     # Convert the news stories to a dataframe
     logger.info(f"Converting all news stories to a structured format.")
     struc_newsstories = StructuredNewsStories(list_news_stories=list_news_stories)
@@ -34,4 +34,4 @@ def runner(list_news_stories: List[NewsStory]) -> pd.DataFrame:
     # Add metadata and calculate the score
     struc_newsstories.add_all_metadata_and_score(all_target_fields=target_fields)
 
-    return struc_newsstories.df_news_stories
+    return struc_newsstories.df_news_stories, target_fields
