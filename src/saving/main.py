704f5b0166b1910ca60b07df79663a10c78d521f
Ben Crestel <ben.crestel@zoho.com>
2025-01-09 17:20:21 -0500

    add unique identifier before saving to disk + add tests

--- src/saving/main.py
+++ src/saving/main.py
@@ -3,19 +3,53 @@ import logging
 import pandas as pd
 
 logger = logging.getLogger(__name__)
+
+# TODO: review PATH_TO_FILE. Could be log/ folder at root, and that folder is mounted in the docker file
 PATH_TO_FILE = "'../../secrets_vault/scored_news_stories.parquet'"
+UNIQUE_ID_COL = "unique_id"
 
 
 def runner(
     df_scored_news_stories_new: pd.DataFrame, path_to_file: str = PATH_TO_FILE
 ) -> None:
+    """Add unique_id to df_scored_news_stories_new and add to existing db (if any)
+
+    Args:
+        df_scored_news_stories_new (pd.DataFrame): current dataframe of news stories
+        path_to_file (str, optional): path to the existing db. Defaults to PATH_TO_FILE.
+
+    Raises:
+        KeyError: if existing database doesn't have unique_id column. This needs to be fixed manually.
+    """
+    # Load existing db
     try:
         df_scored_news_stories_existing = pd.read_parquet(path_to_file)
-        df_scored_news_stories = pd.concat(
-            [df_scored_news_stories_existing, df_scored_news_stories_new],
-            ignore_index=True,
+    except FileNotFoundError as e:
+        logger.warning(f"Could not find {path_to_file}. Starting from scratch.")
+        df_scored_news_stories_existing = None
+    # Add a unique identifier to the next dataframe
+    if df_scored_news_stories_existing is None:
+        start_unique_id = 0
+    else:
+        if UNIQUE_ID_COL not in df_scored_news_stories_existing.columns:
+            logger.error(
+                f"Could not find column in {UNIQUE_ID_COL} in existing dataframe. "
+                + f"Therefore, cannot create a {UNIQUE_ID_COL} for the new data. "
+                + f"Fix this by adding and populating a {UNIQUE_ID_COL} column in {PATH_TO_FILE}."
+            )
+            raise KeyError(
+                f"Could not find column {UNIQUE_ID_COL} in existing dataframe: {df_scored_news_stories_existing.columns}"
+            )
+        start_unique_id = (
+            df_scored_news_stories_existing[UNIQUE_ID_COL].max().item() + 1
         )
-    except:
-        logger.warning(f"Could not read {path_to_file}. Starting from scratch.")
-        df_scored_news_stories = df_scored_news_stories_new
+    df_scored_news_stories_new[UNIQUE_ID_COL] = range(
+        start_unique_id, start_unique_id + len(df_scored_news_stories_new)
+    )
+    # Combine new and existing dataframes
+    df_scored_news_stories = pd.concat(
+        [df_scored_news_stories_existing, df_scored_news_stories_new],
+        axis=0,
+        ignore_index=True,
+    )
     df_scored_news_stories.to_parquet(path_to_file)
---
+++ src/tests/saving/test_main.py
@@ -0,0 +1,94 @@
+import os
+import shutil
+
+import pandas as pd
+import pytest
+
+from src.saving.main import UNIQUE_ID_COL, runner
+
+PATH_TO_LOG_PARQUET = "/home/src/tests/data/test_log_report.parquet"
+TMP_PATH_TO_PARQUET = "test.parquet"
+
+
+def prep_no_file(path_tmp):
+    df_log_report = pd.read_parquet(PATH_TO_LOG_PARQUET)
+    runner(df_scored_news_stories_new=df_log_report, path_to_file=path_tmp)
+    df_created_by_runner = pd.read_parquet(path_tmp)
+    return df_log_report, df_created_by_runner
+
+
+def test_no_file_1(tmp_path):
+    # Check we have the same dataframes before and after
+    df_raw = pd.read_parquet(PATH_TO_LOG_PARQUET)
+    df_log_report, df_created_by_runner = prep_no_file(tmp_path / TMP_PATH_TO_PARQUET)
+    assert len(df_raw) == len(df_log_report)
+    assert len(df_raw.columns) + 1 == len(df_log_report.columns)
+    assert (
+        df_log_report.shape == df_created_by_runner.shape
+    ), f"{df_log_report.shape} vs {df_created_by_runner.shape}"
+    assert df_log_report.equals(df_created_by_runner)
+
+
+def test_no_file_2(tmp_path):
+    # Check unique_id is in the right dataframes
+    df_raw = pd.read_parquet(PATH_TO_LOG_PARQUET)
+    assert UNIQUE_ID_COL not in df_raw.columns, df_raw.columns
+    df_log_report, df_created_by_runner = prep_no_file(tmp_path / TMP_PATH_TO_PARQUET)
+    assert UNIQUE_ID_COL in df_log_report.columns, df_log_report.columns
+    assert UNIQUE_ID_COL in df_created_by_runner.columns, df_created_by_runner.columns
+
+
+def test_no_file_3(tmp_path):
+    # Check unique_id is unique
+    _, df_created_by_runner = prep_no_file(tmp_path / TMP_PATH_TO_PARQUET)
+    unique_ids = df_created_by_runner[UNIQUE_ID_COL].unique()
+    assert len(unique_ids) == len(df_created_by_runner)
+
+
+def prep_existing_file(path_tmp, add_unique_id_col=False):
+    if add_unique_id_col:
+        df_tmp = pd.read_parquet(PATH_TO_LOG_PARQUET)
+        df_tmp[UNIQUE_ID_COL] = range(len(df_tmp))
+        df_tmp.to_parquet(path_tmp)
+    else:
+        shutil.copy(PATH_TO_LOG_PARQUET, path_tmp)
+    df_log_report = pd.read_parquet(path_tmp)
+    df_news_stories = pd.read_parquet(PATH_TO_LOG_PARQUET)
+    runner(df_scored_news_stories_new=df_news_stories, path_to_file=path_tmp)
+    df_created_by_runner = pd.read_parquet(path_tmp)
+    return df_log_report, df_created_by_runner
+
+
+def test_existing_file_1(tmp_path):
+    # Check this raises an error as the UNIQUE_ID_COL is not defined
+    with pytest.raises(KeyError):
+        _, _ = prep_existing_file(tmp_path / TMP_PATH_TO_PARQUET)
+
+
+def test_existing_file_2(tmp_path):
+    # Check dimensions of the dataframes before and after
+    df_log_report, df_created_by_runner = prep_existing_file(
+        tmp_path / TMP_PATH_TO_PARQUET, add_unique_id_col=True
+    )
+    assert len(df_log_report.columns) == len(
+        df_created_by_runner.columns
+    ), f"{df_log_report.columns} vs {df_created_by_runner.columns}"
+    assert 2 * len(df_log_report) == len(df_created_by_runner)
+
+
+def test_existing_file_2(tmp_path):
+    # Check unique_id is in both dataframes
+    df_log_report, df_created_by_runner = prep_existing_file(
+        tmp_path / TMP_PATH_TO_PARQUET, add_unique_id_col=True
+    )
+    assert UNIQUE_ID_COL in df_log_report.columns, df_log_report.columns
+    assert UNIQUE_ID_COL in df_created_by_runner.columns, df_created_by_runner.columns
+
+
+def test_existing_file_3(tmp_path):
+    # Check unique_id is unique
+    _, df_created_by_runner = prep_existing_file(
+        tmp_path / TMP_PATH_TO_PARQUET, add_unique_id_col=True
+    )
+    unique_ids = df_created_by_runner[UNIQUE_ID_COL].unique()
+    assert len(unique_ids) == len(df_created_by_runner)
