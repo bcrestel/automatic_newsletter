2119c2baa2496df7b0f77c35e5d4f5c7aee1566b
Ben Crestel <ben.crestel@zoho.com>
2025-01-30 18:07:01 -0500

    finish GenAIModel class

--- src/genai_model/genai_model.py
+++ src/genai_model/genai_model.py
@@ -5,6 +5,9 @@ from litellm.types.utils import ModelResponse
 from litellm.litellm_core_utils.streaming_handler import CustomStreamWrapper
 from litellm import completion as litellm_completion
 
+from src.genai_model.config import MODELS_CATALOG
+from src.utils.list import flatten_list_of_lists
+
 logger = logging.getLogger(__name__)
 
 
@@ -13,51 +16,12 @@ class GenAIModel:
         self.model_type = model_type
         defaut_system_prompt = "You are a helpful assistant."
         self.system_prompt = system_promt if len(system_promt) > 0 else defaut_system_prompt
-        self.catalog_models = {
-            "moderation": [
-                "groq/llama-guard-3-8b",
-                "mistral/mistral-moderation-2411",
-            ],
-            "reasoning": [
-                "gemini/gemini-2.0-flash-thinking-exp-01-21",
-                "openrouter/google/gemini-2.0-flash-thinking-exp:free",
-                "gemini/gemini-2.0-flash-thinking-exp-1219",
-                "openrouter/google/gemini-2.0-flash-thinking-exp-1219:free",
-                "openrouter/deepseek/deepseek-r1:free",
-            ],
-            "large": [
-                "gemini/gemini-1.5-pro-002",
-                "gemini/models/gemini-1.5-pro-exp-0827",
-                "openrouter/meta-llama/llama-3.1-405b-instruct:free",
-                "gemini/gemini-1.5-pro-001",
-                "mistral/mistral-large-2411",
-                "mistral/mistral-large-2407",
-                "mistral/mistral-large-2402",
-            ],
-            "medium": [
-                "gemini/gemini-1.5-flash-latest",
-                "gemini/gemini-1.5-flash-002",
-                "gemini/gemini-1.5-flash-exp-0827"
-                "groq/llama-3.3-70b-versatile",
-                "groq/llama-3.3-70b-specdec",
-                "gemini/gemini-1.5-flash-001",
-                "mistral/mistral-medium",
-            ],
-            "small": [
-                "gemini/gemini-1.5-flash-8b-001",
-                "gemini/gemini-1.5-flash-8b-latest",
-                "gemini/gemini-1.5-flash-8b-exp-0924",
-                "gemini/gemini-1.5-flash-8b-exp-0827",
-                "mistal/ministral-8b-2410",
-                "openrouter/meta-llama/llama-3.1-8b-instruct:free",
-                "groq/llama-3.1-8b-instant",
-                "openrouter/meta-llama/llama-3-8b-instruct:free",
-            ],
-        }
-        self.model_list = self._map_model_type_to_model_names(self.model_type)
+        self.models_catalog = MODELS_CATALOG
+        self.all_models = flatten_list_of_lists((self.models_catalog.values()))
+        self.list_of_models = self._map_model_type_to_model_names(self.model_type)
     
-    def completion(self, user_prompt: str, parameters: dict = {}) -> Union[ModelResponse, CustomStreamWrapper]:
-        for model in self.model_list:
+    def completion(self, user_prompt: str, parameters: dict = {"temperature": 1.0}) -> Union[ModelResponse, CustomStreamWrapper]:
+        for model in self.list_of_models:
             try:
                 return litellm_completion(
                     model=model,
@@ -68,9 +32,10 @@ class GenAIModel:
                     **parameters,
                     )
             except Exception as e:
-                logger.warning(f"Model {model} raised an Exception")
+                logger.warning(f"Model {model} raised an Exception.")
+                logger.debug(e)
         logger.error("All models in the list raised an Exception: {self.model_list}")
-        raise ValueError(f"No more models to try")
+        raise ValueError(f"No more models left to try.")
     
     def completion_str(self, *args, **kwargs) -> str:
         return self.get_content_from_response(self.completion_str(*args, **kwargs))
@@ -79,15 +44,40 @@ class GenAIModel:
         return response['choices'][0].message.content
     
     def get_all_model_types(self):
-        return self.catalog_models.keys()
+        return self.models_catalog.keys()
 
     def _map_model_type_to_model_names(self, model_type: str) -> List[str]:
-        try:
-            list_of_models = self.catalog_models[model_type]
-        except KeyError as e:
-            logger.error(f"Values allowed for model_type are: {self.get_all_model_types()}")
-            raise e
-        # tmp
+        """Collect the list of models specified by model_type. This can be done in 3 different ways:
+        - specific category: 'reasoning', 'large', 'medium', 'small', 'moderation'
+        - all: use all models in 'large', 'medium', 'small'
+        - specific model name: will collect all models found in the entier catalog
+
+        Args:
+            model_type (str): key word to identify the targeted collection of models
+
+        Raises:
+            NotImplementedError: if model_type was incorrect
+
+        Returns:
+            List[str]: list of models corresponding to that model_type
+        """
+        list_of_models = []
+        if model_type in self.models_catalog.keys():
+            list_of_models = self.models_catalog[model_type]
+        elif model_type == "all":
+            list_of_models = self.models_catalog["large"] \
+                + self.models_catalog["medium"] \
+                + self.models_catalog["small"] 
+        else:
+            for _, model_list in self.models_catalog.items():
+                for model in model_list:
+                    if model_type in model:
+                        list_of_models.append(model)
+        # Check we have found models for that model_type
         if len(list_of_models) == 0:
+            logger.error(f"Could not find any models corresponding to model_type: {model_type}")
             raise NotImplementedError
+        else:
+            logger.info(f"Found {len(list_of_models)} models for model_type {model_type}")
+            logger.info(f"List of models included: {list_of_models}")
         return list_of_models
