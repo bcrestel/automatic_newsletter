59b106bffeecd738fcc6253fe0d4d0eed5b4cc5b
Ben Crestel <ben.crestel@zoho.com>
2025-02-11 01:34:17 -0500

    fix AlphaSignal parser

--- src/newsletters/config.py
+++ src/newsletters/config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from src.newsletters.parser.alpha_signal import alpha_signal_parser
+from src.newsletters.parser.bloomberg_tech import bloomberg_tech_parser
 from src.newsletters.parser.tldr import tldr_parser
 
 NEWSLETTER_AND_PARSER = {
@@ -8,4 +9,5 @@ NEWSLETTER_AND_PARSER = {
     "AlphaSignal <news@alphasignal.ai>": alpha_signal_parser,
     "TLDR <dan@tldrnewsletter.com>": tldr_parser,
     "TLDR Product <dan@tldrnewsletter.com>": tldr_parser,
+    "Bloomberg Technology <noreply@news.bloomberg.com>": bloomberg_tech_parser,
 }
--- src/newsletters/parser/alpha_signal.py
+++ src/newsletters/parser/alpha_signal.py
@@ -2,8 +2,6 @@ import logging
 import re
 from typing import List
 
-import emoji
-
 from src.genai_model.genai_model import GenAIModel
 from src.news_story import NewsStory
 from src.newsletters.email import Email
@@ -19,6 +17,7 @@ SECTIONS = [
 ]
 TOP_NEWS_START = "WHAT'S NEW"
 TOP_NEWS_END = "TRY NOW"
+SPLIT_LINES = "\r\n\r\n"
 SPLIT_PATTERN = "\r\n\r\n \t*\r\n"
 
 
@@ -33,17 +32,21 @@ def alpha_signal_parser(email: Email) -> List[NewsStory]:
     """
     logger.info(f"Parsing email '{email['subject']}'")
     txt = email["text"]
-    sections = []
-    for sec in SECTIONS:
-        _tmp = txt.split(sec)
-        sections.append(_tmp[0])
-        txt = _tmp[1]
-    sections = sections[1:]
+    lines = txt.split("TOP NEWS")[1].split(SPLIT_LINES)
     news_stories = []
-    for section_idx, section in enumerate(sections):
-        articles = section.split(SPLIT_PATTERN)
-        for art in articles[1:]:
-            txt = emoji.replace_emoji(art, replace="").replace("\r\n", " ").strip()
+    for line in lines:
+        if "\t*\r" in line and re.search(r"\[\d+\]", line):
+            txt = line.lstrip(" \t*\r\n").replace("\r\n", " ").strip()
+            #    sections = []
+            #    for sec in SECTIONS:
+            #        _tmp = txt.split(sec)
+            #        sections.append(_tmp[0])
+            #        txt = _tmp[1]
+            #    sections = sections[1:]
+            #    for section_idx, section in enumerate(sections):
+            #        articles = section.split(SPLIT_PATTERN)
+            #        for art in articles[1:]:
+            #            txt = emoji.replace_emoji(art, replace="").replace("\r\n", " ").strip()
             ns = NewsStory(
                 title=txt.split("[")[0].strip(),
                 url=txt.split("[")[1].split("]")[0],
@@ -53,7 +56,7 @@ def alpha_signal_parser(email: Email) -> List[NewsStory]:
                 date_source=email["date_utc"],
                 date_source_time_zone="utc",
             )
-            if section_idx == 0 and len(articles) == 2:
+            if len(news_stories) == 0:
                 ns["text"] = get_text_for_top_news(email["text"])
                 summarizer = GenAIModel(model_type="small")
                 summary = summarizer.completion_str(
@@ -73,5 +76,6 @@ def get_text_for_top_news(email_text: str) -> str:
         .split(TOP_NEWS_START)[1]
         .split(SECTIONS[1])[0]
         .split(TOP_NEWS_END)[0]
+        .replace("\r\n", " ")
         .strip()
     )
